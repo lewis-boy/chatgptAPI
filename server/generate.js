bc8da86f4e16a7798bdeb9c4540c59275c3ca757
Luis Escobar <luisaeniebla@gmail.com>
2023-06-22 16:47:24 -0700

    We updated to the chatgpt model because it is cheaper and more sophisticated

--- server/generate.js
+++ server/generate.js
@@ -1,13 +1,37 @@
 import openai from './server.js';
 
 const generate = async(query) => {
-    const res = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `Convert the following natural language description into a SQL query: \n\n${query}.`,
-        max_tokens: 100,
-        temperature: 0
-    });
-    return res.data.choices[0].text;
+
+    const davinci = async(query) => {
+        const res = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Convert the following natural language description into a SQL query: \n\n${query}.`,
+            max_tokens: 100,
+            temperature: 0
+        });
+        return res.data.choices[0].text;
+    };
+
+    const chatgptApi = async(query) => {
+        const messages = [
+            {role: "system", content: `You are a translator from plain English to SQL.`},
+            {role: "user", content: `Convert the following natural language description into a SQL query: \n\nshow all elements from the table users.`,},
+            {role: "assistant", content: `SELECT * FROM users;`},
+            {role: "user", content: `Convert the following natural language description into a SQL query: \n\n${query}.`}
+        ];
+
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+
+        });
+        
+        return response.data.choices[0].message.content;
+    }
+
+
+    return await chatgptApi(query);
+
 }
 
 export default generate;
